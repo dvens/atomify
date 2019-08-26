7747c13115b883e37abb22b30b81762a00f4eb83
dvens <d.joelvens@gmail.com>
2019-08-26 19:28:43 +0200

    changed componentwillLoad order

--- packages/core/src/component/connected-callback.ts
+++ packages/core/src/component/connected-callback.ts
@@ -6,10 +6,12 @@ import { updateComponent, safeCall } from '../component';
 export const connectedCallback = async( target: ComponentConstructor, options: ComponentOptions, instance: any ) => {
 
     safeCall( target, instance, 'connectedCallback' );
+
+    await setInitialPropertyValues( target, instance );
+
     safeCall( target, instance, 'componentWillLoad' );
     safeCall( target, instance, 'componentWillRender' );
 
-    await setInitialPropertyValues( target, instance );
     await initializePropertyToAttributes( target );
     await updateComponent( target, options );
     await addRemoveEventListeners( target );
