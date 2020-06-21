e425113224e4878d4ad20aacd4c2543dcfe26e1f
dvens <d.joelvens@gmail.com>
2020-06-21 13:46:24 +0200

    WIP: Rework atomify/jsx

--- packages/jsx/src/core/apply-attributes.ts
+++ packages/jsx/src/core/apply-attributes.ts
@@ -1,30 +1,11 @@
-import { isCustomElement } from '../utilities';
-
-const BOOLEAN_ATTRS = [
-    'async',
-    'allowfullscreen',
-    'allowpaymentrequest',
-    'autofocus',
-    'autoplay',
-    'checked',
-    'controls',
-    'default',
-    'defer',
-    'disabled',
-    'formnovalidate',
-    'hidden',
-    'ismap',
-    'multiple',
-    'muted',
-    'novalidate',
-    'nowrap',
-    'open',
-    'readonly',
-    'required',
-    'reversed',
-    'selected',
-    'playsinline',
-];
+import {
+    FunctionValue,
+    isBooleanAttr,
+    isCustomElement,
+    isFunction,
+    removeAttr,
+    setAttr,
+} from '../utilities';
 
 const XLINK_NS = 'http://www.w3.org/1999/xlink';
 
@@ -37,48 +18,68 @@ export const applyAttributes = (element: any, vnodeData: object) => {
 
         let lAttributeName = attribute.toLowerCase();
 
-        if (attributeName === 'style') {
-            return Object.assign(element.style, attributeValue);
-        } else if (attributeName === 'class' || attributeName === 'className') {
-            return element.setAttribute('class', attributeValue);
-        } else if (attributeName === 'htmlFor') {
-            return element.setAttribute('for', attributeValue);
-        } else if ((attributeName as string).indexOf('on') === 0) {
-            const eventName = (attributeName as string).substr(2).toLowerCase();
-            return element.addEventListener(eventName, attributeValue);
-        } else if (
-            isCustomElement(element) &&
-            !(attributeName as string).includes('-') &&
-            !BOOLEAN_ATTRS.includes(attributeName) &&
-            typeof element.constructor.properties !== 'undefined' &&
-            element.constructor.properties.has(attributeName)
-        ) {
-            return (element[attributeName] = attributeValue);
-        } else if (attributeName === 'dangerouslySetInnerHTML') {
-            return (element.innerHTML = attributeValue);
-        } else if (BOOLEAN_ATTRS.includes(attributeName)) {
-            if (attributeValue) {
-                return element.setAttribute(attributeName, '');
-            } else {
-                return element.removeAttribute(attributeName);
-            }
-        } else if (typeof attributeValue !== 'function') {
-            if (lAttributeName !== (lAttributeName = lAttributeName.replace(/^xlink\:?/, ''))) {
-                if (attributeValue == null || attributeValue === false) {
-                    return element.removeAttributeNS(XLINK_NS, lAttributeName.toLowerCase());
+        switch (attributeName) {
+            case 'style':
+                Object.assign(element.style, attributeValue);
+                return;
+            case 'class':
+            case 'className':
+                if (isFunction(attributeValue)) {
+                    (attributeValue as FunctionValue)(element);
                 } else {
-                    return element.setAttributeNS(
-                        XLINK_NS,
-                        lAttributeName.toLowerCase(),
-                        attributeValue,
-                    );
+                    setAttr(element, 'style', attributeValue);
                 }
-            } else if (attributeValue == null || attributeValue === false) {
-                return element.removeAttribute(attributeName);
+                return;
+            case 'htmlFor':
+                setAttr(element, 'for', attributeValue);
+                return;
+            case 'dangerouslySetInnerHTML':
+                element.innerHTML = attributeValue;
+                return;
+        }
+
+        if (isBooleanAttr(attributeName)) {
+            if (attributeValue) {
+                setAttr(element, attributeName, '');
             } else {
-                const isBooleanValue = typeof attributeValue === 'boolean';
-                return element.setAttribute(attributeName, isBooleanValue ? '' : attributeValue);
+                removeAttr(element, attributeName);
             }
+            return;
+        } else if (
+            isFunction(attributeValue) &&
+            attributeName[0] === 'o' &&
+            attributeName[1] === 'n'
+        ) {
+            const eventName = (attributeName as string).substr(2).toLowerCase();
+            element.addEventListener(eventName, attributeValue);
+        }
+
+        // if (
+        //     (isCustomElement(element) &&
+        //         !(attributeName as string).includes('-') &&
+        //         !isBooleanAttr(attributeName) &&
+        //         typeof element.constructor.properties !== 'undefined' &&
+        //         element.constructor.properties.has(attributeName)) ||
+        //     (element.props && attributeName in element.props)
+        // ) {
+        //     return (element[attributeName] = attributeValue);
+        // } else if (typeof attributeValue !== 'function') {
+        //     if (lAttributeName !== (lAttributeName = lAttributeName.replace(/^xlink\:?/, ''))) {
+        //         if (attributeValue == null || attributeValue === false) {
+        //             return element.removeAttributeNS(XLINK_NS, lAttributeName.toLowerCase());
+        //         } else {
+        //             return element.setAttributeNS(
+        //                 XLINK_NS,
+        //                 lAttributeName.toLowerCase(),
+        //                 attributeValue,
+        //             );
+        //         }
+        //     } else if (attributeValue == null || attributeValue === false) {
+        //         return element.removeAttribute(attributeName);
+        //     } else {
+        //         const isBooleanValue = typeof attributeValue === 'boolean';
+        //         return element.setAttribute(attributeName, isBooleanValue ? '' : attributeValue);
+        //     }
         }
     });
 
--- packages/jsx/src/utilities/index.ts
+++ packages/jsx/src/utilities/index.ts
@@ -1,2 +1,9 @@
 export * from './is-svg';
 export * from './is-custom-element';
+export * from './is-boolean-attr';
+
+export type FunctionValue = (...args: any) => void;
+export const isFunction = (value: string) => typeof value === 'function';
+export const setAttr = (node: Element, attrName: string, attrValue: string) =>
+    node.setAttribute(attrName, attrValue);
+export const removeAttr = (node: Element, attrName: string) => node.removeAttribute(attrName);
